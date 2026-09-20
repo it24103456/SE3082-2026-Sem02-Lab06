4ab72460037d136ca0ea023c1440b9b58b108454
it24103456 <it24103456@my.sliit.lk>
2026-09-20 22:23:57 +0530

    mandelbrot omp

---
+++ Exercise07/mandelbrot_omp.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <omp.h>
+
+#define NPOINTS 2000
+#define MAXITER 2000
+
+struct complex {
+  double real;
+  double imag;
+};
+
+int main() {
+  int numoutside = 0;
+
+  double t1 = omp_get_wtime();
+
+  #pragma omp parallel reduction(+:numoutside)
+  {
+    struct complex z, c;
+    double ztemp;
+
+    #pragma omp for schedule(static)
+    for (int i = 0; i < NPOINTS; i++) {
+      for (int j = 0; j < NPOINTS; j++) {
+        c.real = -2.0 + 2.5 * (double)i / (double)NPOINTS + 1.0e-7;
+        c.imag =  1.125 * (double)j / (double)NPOINTS + 1.0e-7;
+        z = c;
+
+        for (int iter = 0; iter < MAXITER; iter++) {
+          ztemp = (z.real * z.real) - (z.imag * z.imag) + c.real;
+          z.imag = z.real * z.imag * 2.0 + c.imag;
+          z.real = ztemp;
+
+          if ((z.real * z.real + z.imag * z.imag) > 4.0) {
+            numoutside++;
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  double t2 = omp_get_wtime();
+
+  double area = 2.0 * 2.5 * 1.125 *
+                (double)(NPOINTS * NPOINTS - numoutside) /
+                (double)(NPOINTS * NPOINTS);
+
+  double error = area / (double)NPOINTS;
+
+  printf("Area of Mandlebrot set = %12.8f +/- %12.8f\n", area, error);
+  printf("Time = %f seconds\n", t2 - t1);
+
+  return 0;
+}
